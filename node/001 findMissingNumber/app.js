112cac276558e0693f3465aae2a0df67ad80ae3c
maneeshhm <maneeshdil95@gmail.com>
2022-06-04 11:54:22 +0530

    complete node assignment-change directory

--- node/001 findMissingNumber/app.js
+++ node/001 findMissingNumber/app.js
@@ -5,7 +5,7 @@ let  randomNum = Math.floor(Math.random
     ()*100);
 
 
-const numberseq = [];
+const numberseq = [21,22,23,24,25,26,27,28,29];
 
 //adding values to array
 for (let index = 0; index < 10; index++) {
@@ -27,6 +27,7 @@ numberseq.splice(Math.floor(Math.random()*10),1);
 for (let x = 0; x < numberseqModi.length; x++) {
     
     if(!numberseq.includes(numberseqModi[x])){
+        console.log("+++++++++++++++++++++");
         console.log("Missing value is: " + numberseqModi[x] )
     
         
@@ -34,3 +35,8 @@ for (let x = 0; x < numberseqModi.length; x++) {
     
     
 }
+
+console.log("+++++++++++++++++++++");
+console.log(numberseqModi);
+console.log("+++++++++++++++++++++");
+console.log(numberseq);
