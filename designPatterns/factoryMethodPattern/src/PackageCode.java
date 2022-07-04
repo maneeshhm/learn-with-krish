3c89537c70911612238e45090d22860759fae96d
maneeshhm <maneeshdil95@gmail.com>
2022-07-04 22:28:04 +0530

    re-uploaded folders

---
+++ designPatterns/factoryMethodPattern/src/PackageCode.java
@@ -0,0 +1,3 @@
+public enum PackageCode {
+    SILVER,GOLD,PLATINUM
+}
---
+++ designPatterns/factoryMethodPattern/src/SilverPackage.java
@@ -0,0 +1,7 @@
+public class SilverPackage extends Package{
+
+    @Override
+    protected void createPackage() {
+        travelList.add(new Camping());
+    }
+}
